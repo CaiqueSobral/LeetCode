c0cf32b8fd243cc2902b7483d3e292fdf78f86be
Caique Sobral <caiquelsobral@gmail.com>
2023-08-07 08:10:46 -0300

    1 - Two Sum

---
+++ 1-TwoSum.ts
@@ -0,0 +1,24 @@
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers 
+such that they add up to target.
+
+You may assume that each input would have exactly one solution, 
+and you may not use the same element twice.
+
+You can return the answer in any order.
+*/
+
+function twoSum(nums: number[], target: number): number[] {
+  const map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    const howMuchToTarget = target - nums[i];
+
+    if (map.has(howMuchToTarget)) {
+      return [map.get(howMuchToTarget), i];
+    }
+
+    map.set(nums[i], i);
+  }
+
+  return [];
+}
